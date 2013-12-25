0684eb057ebb44edff54bf098bb3335949c42e9a
MangoFYang <18729218113@163.com>
2013-12-25 22:10:38 +0800

    Update IndexController.java
    mapping for index

--- xiang-web Maven Webapp/src/main/java/com/yangfan/xiang/web/IndexController.java
+++ xiang-web Maven Webapp/src/main/java/com/yangfan/xiang/web/IndexController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+	/**
+	 * 
+	 * mapping for index
+	 * 
+	 */
 	@RequestMapping("/index")
 	public String index() {
 		return "/index";
