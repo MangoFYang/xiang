4c4d1533721a95e490a1c146aeac2438b17a0790
MangoFYang <18729218113@163.com>
2014-01-02 14:16:56 +0800

    unit test mybatis style and refact dev.properties

---
+++ xiang-demo-persist/src/test/java/com/yangfan/xiang/persist/mapper/demo/emp/DeptMapperTest.java
@@ -0,0 +1,24 @@
+package com.yangfan.xiang.persist.mapper.demo.emp;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+
+import com.yangfan.xiang.model.po.demo.emp.Dept;
+import com.yangfan.xiang.persist.BasePersistTest;
+
+public class DeptMapperTest extends BasePersistTest {
+	
+	@Resource
+	private DeptMapper deptMapper;
+
+	@Test
+	public void testSavePojo() {
+		Dept dept = new Dept();
+		dept.setDeptNo(1005);
+		dept.setDeptName("name1005");
+		dept.setLoc("loc1005");
+		deptMapper.savePojo(dept);
+	}
+
+}
