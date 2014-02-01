b4983a78912c862db126d927a7c9e83802d6dee7
MangoFYang <18729218113@163.com>
2014-02-02 01:18:57 +0800

    init

---
+++ xiang-demo-controller/src/main/java/com/yangfan/xiang/controller/demo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ * 此目录存放demo controller层代码
+ * 
+ * @author 杨帆
+ *
+ */
+package com.yangfan.xiang.controller.demo;
---
+++ xiang-demo-controller/src/main/java/com/yangfan/xiang/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ * 此目录存放控制器层代码
+ * 
+ * @author 杨帆
+ *
+ */
+package com.yangfan.xiang.controller;
