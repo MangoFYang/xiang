1db7496b1d7db6e3f164cdac7ed606fd4f9a1513
MangoFYang <18729218113@163.com>
2014-02-06 21:55:03 +0800

    first data integration, demo.foo

--- xiang-demo-service/src/test/java/com/yangfan/xiang/service/impl/demo/abc/FooServiceImplTest.java
+++ xiang-demo-service/src/test/java/com/yangfan/xiang/service/impl/demo/abc/FooServiceImplTest.java
@@ -1,9 +1,16 @@
 package com.yangfan.xiang.service.impl.demo.abc;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
+import org.springframework.data.domain.Page;
 
+import com.yangfan.xiang.core.web.PageRequest;
 import com.yangfan.xiang.model.po.demo.abc.Foo;
 import com.yangfan.xiang.service.iface.demo.abc.FooService;
 import com.yangfan.xiang.test.CoreTest;
@@ -17,6 +24,29 @@ public class FooServiceImplTest extends CoreTest {
 	public void testFindOne() {
 		Foo findOne = fooService.findOne(1L);
 		System.out.println(findOne);
+	}
+	
+	@Test
+	public void testFindAllPageable() {
+		Page<Foo> findAll = fooService.findAll(new PageRequest(1, 2));
+		System.out.println(findAll);
+	}
+	
+	@Test
+	public void test() {
+		List<Foo> fooList = new ArrayList<Foo>();
+		for (int i = 0; i < 100; i++) {
+			Foo foo = new Foo();
+			foo.setBigDecimalField(new BigDecimal("3.693693").add(new BigDecimal(i)));
+			foo.setDateField(new Date());
+			foo.setDoubleBoxField(1.2 + i);
+			foo.setDoubleField(1.999 + i);
+			foo.setIntegerField(9000 + i);
+			foo.setIntField(999 + i);
+			foo.setStringField("111strstr111" + i);
+			fooList.add(foo);
+		}
+		fooService.save(fooList);
 	}
 
 }
