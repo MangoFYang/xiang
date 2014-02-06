a828cdd26955981bbe46f92004869eb77e01169e
MangoFYang <18729218113@163.com>
2014-02-06 21:55:03 +0800

    first data integration, demo.foo

--- xiang-demo-controller/src/main/java/com/yangfan/xiang/controller/demo/abc/FooController.java
+++ xiang-demo-controller/src/main/java/com/yangfan/xiang/controller/demo/abc/FooController.java
@@ -1,46 +1,28 @@
 package com.yangfan.xiang.controller.demo.abc;
 
+import javax.annotation.Resource;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Controller接收并处理客户端的请求。
- * 
- * 请求映射为：/demo/abc/FooController，首个"/"代表项目根路径，比如：http://localhost:8080/xiang-web
- * 
- * Controller访问地址为：http://localhost:8080/xiang-web/demo/abc/FooController
- * 
- * @author 杨帆
- *
- */
+import com.yangfan.xiang.model.po.demo.abc.Foo;
+import com.yangfan.xiang.service.iface.demo.abc.FooService;
+
 @Controller
 @RequestMapping("/demo/abc/FooController")
 public class FooController {
+	
+	@Resource
+	private FooService fooService;
 
-	/**
-	 * 请求映射为：/index
-	 * 
-	 * 此方法访问地址 = Controller访问地址 + 此方法的请求映射，如下：
-	 * 
-	 * http://localhost:8080/xiang-web/demo/abc/FooController/index
-	 * 
-	 * 返回字符串类型，代表视图资源。
-	 * 
-	 * @return 视图路径
-	 */
 	@RequestMapping("/index")
-	public String index() {
-		
-		/*
-		 * 返回视图资源。
-		 * 项目中所有视图(JSP页面)都放在src/main/webapp/WEB-INF/views/jstl文件夹中，
-		 * 所以返回的路径只需从此目录开始写起即可，
-		 * 注意：必须以"/"开头，不用以".jsp"结尾。
-		 * 
-		 * 最后指向完整的JSP页面资源为：
-		 * src/main/webapp/WEB-INF/views/jstl/demo/abc/index.jsp
-		 */
-		return "/demo/abc/index";
+	@ResponseBody
+	public Page<Foo> index(Pageable pageable) {
+		Page<Foo> findAll = fooService.findAll(pageable);
+		return findAll;
 	}
 	
 }
--- xiang-demo-service/src/test/java/com/yangfan/xiang/service/impl/demo/abc/FooServiceImplTest.java
+++ xiang-demo-service/src/test/java/com/yangfan/xiang/service/impl/demo/abc/FooServiceImplTest.java
@@ -3,7 +3,9 @@ package com.yangfan.xiang.service.impl.demo.abc;
 import javax.annotation.Resource;
 
 import org.junit.Test;
+import org.springframework.data.domain.Page;
 
+import com.yangfan.xiang.core.web.PageRequest;
 import com.yangfan.xiang.model.po.demo.abc.Foo;
 import com.yangfan.xiang.service.iface.demo.abc.FooService;
 import com.yangfan.xiang.test.CoreTest;
@@ -17,6 +19,12 @@ public class FooServiceImplTest extends CoreTest {
 	public void testFindOne() {
 		Foo findOne = fooService.findOne(1L);
 		System.out.println(findOne);
+	}
+	
+	@Test
+	public void testFindAllPageable() {
+		Page<Foo> findAll = fooService.findAll(new PageRequest(1, 2));
+		System.out.println(findAll);
 	}
 
 }
